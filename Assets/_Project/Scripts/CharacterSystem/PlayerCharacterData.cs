8e8ac063a60919db2f7140d3a36f46142b85706d
Abdülkadir Dursun <akadirdursn@gmail.com>
2025-06-12 16:31:20 +0300

    feat: Add level and experience fields to player character data

--- Assets/_Project/Scripts/CharacterSystem/PlayerCharacterData.cs
+++ Assets/_Project/Scripts/CharacterSystem/PlayerCharacterData.cs
@@ -6,17 +6,35 @@ namespace MightyAdventures.CharacterSystem
     [CreateAssetMenu(fileName = "PlayerCharacterData", menuName = "Mighty Adventures/Character System/Player Character Data")]
     public class PlayerCharacterData : ScriptableObject
     {
-        [SerializeField] private string playerName;
+        [SerializeField] private string characterName;
+        [SerializeField] private int level = 1;
+        [SerializeField] private int experience;
         [SerializeField] private CharacterStats characterStats;
         private PlayerCharacterTemplate _template;
+        private int _targetExperience;
+        public string Name => characterName;
+        public int Level => level;
         public GameObject CharacterPrefab => _template.Prefab;
         public CharacterStats CharacterStats => characterStats;
 
         public void InitializePlayerCharacter(PlayerCharacterTemplate template)
         {
             _template = template;
-            playerName = template.CharacterName;
+            characterName = template.CharacterName;
             characterStats = template.CharacterStats.Clone();
+            level = 1;
+            experience = 0;
+            //TODO: Set target experience
+        }
+
+        public void AddExperience(int experienceToAdd)
+        {
+            experience += experienceToAdd;
+            if (experience >= _targetExperience)
+            {
+                level++;
+                //TODO: Set new target experience
+            }
         }
     }
 }
