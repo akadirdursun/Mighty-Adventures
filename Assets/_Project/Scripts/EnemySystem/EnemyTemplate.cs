ae0dc5221921499d7b9177b16c78349498243a72
Abdülkadir Dursun <akadirdursn@gmail.com>
2025-06-12 15:43:44 +0300

    feat: Add enemy templates

---
+++ Assets/_Project/Scripts/EnemySystem/EnemyTemplate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using MightyAdventures.StatSystem;
+using UnityEngine;
+
+namespace MightyAdventures.EnemySystem
+{
+    [CreateAssetMenu(fileName = "NewEnemyTemplate", menuName = "Mighty Adventures/Enemy System/Enemy Template")]
+    public class EnemyTemplate : ScriptableObject
+    {
+        [SerializeField] private string enemyName;
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private EnemyLevelInfo[] levelInfoArray;
+
+        public bool TryGetLevelInfo(int level, out EnemyLevelInfo levelInfo)
+        {
+            EnemyLevelInfo tempLevelInfo = new EnemyLevelInfo();
+            var isAvailable = levelInfoArray.Any(info =>
+            {
+                var available = info.level == level;
+                if (available)
+                    tempLevelInfo = info;
+                return available;
+            });
+            levelInfo = tempLevelInfo;
+            return isAvailable;
+        }
+
+        public bool IsLevelAvailable(int level)
+        {
+            return levelInfoArray.Any(info => info.level == level);
+        }
+    }
+
+    [Serializable]
+    public struct EnemyLevelInfo
+    {
+        public int level;
+        public CharacterStats stats;
+        public int experience;
+    }
+}
