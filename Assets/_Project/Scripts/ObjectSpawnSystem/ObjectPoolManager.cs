e4f12216f6458b3adaed300388cf817259670633
Abdülkadir Dursun <akadirdursn@gmail.com>
2025-06-14 15:01:39 +0300

    feat: Add object pooling system

---
+++ Assets/_Project/Scripts/ObjectSpawnSystem/ObjectPoolManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MightyAdventures.Utilities;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace MightyAdventures.ObjectSpawnSystem
+{
+    public class ObjectPoolManager : Singleton<ObjectPoolManager>
+    {
+        [SerializeField] private ObjectPoolInfo<GameObject>[] objectPools;
+
+        private void Initialize()
+        {
+            foreach (var poolInfo in objectPools)
+            {
+                poolInfo.InitializePool();
+            }
+        }
+
+        #region MonoBehaviour Methods
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+        #endregion
+        
+        #region Structs
+
+        [Serializable]
+        private struct ObjectPoolInfo<T> where T : Object
+        {
+            [SerializeField] private T objectPrefab;
+            [SerializeField] private int startingPoolSize;
+            [SerializeField, Range(0f, 1f)] private float spawnChance;
+            private ObjectPool<T> _pool;
+
+            public float SpawnChance => spawnChance;
+            public ObjectPool<T> Pool => _pool;
+
+            public void InitializePool()
+            {
+                _pool = new ObjectPool<T>(objectPrefab, startingPoolSize);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/_Project/Scripts/Utilities/ObjectPool.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Object = UnityEngine.Object;
+
+namespace MightyAdventures.Utilities
+{
+    [Serializable]
+    public struct ObjectPool<T> where T : Object
+    {
+        private readonly T objectPrefab;
+        private readonly Queue<T> passiveObjects;
+        private readonly List<T> activeObjects;
+        private readonly Action<T,ObjectPool<T>> createAct;
+        private readonly Action<T> getAct;
+        private readonly Action<T> releaseAct;
+        public int PassiveObjectCount => passiveObjects.Count;
+        public int ActiveObjectCount => activeObjects.Count;
+        public IReadOnlyList<T> ActiveObjectList => activeObjects;
+
+        public ObjectPool(T prefab, int startPoolSize, Action<T, ObjectPool<T>> createAct = null, Action<T> getAct = null,
+            Action<T> releaseAct = null)
+        {
+            objectPrefab = prefab;
+            passiveObjects = new Queue<T>(startPoolSize);
+            activeObjects = new List<T>();
+            this.createAct = createAct;
+            this.getAct = getAct;
+            this.releaseAct = releaseAct;
+            for (int i = 0; i < startPoolSize; i++)
+            {
+                passiveObjects.Enqueue(CreateItem());
+            }
+        }
+        
+        public T Get()
+        {
+            T item = PassiveObjectCount != 0 ? passiveObjects.Dequeue() : CreateItem();
+            activeObjects.Add(item);
+            getAct?.Invoke(item);
+            return item;
+        }
+
+        public void Release(T item)
+        {
+            if (!activeObjects.Contains(item)) return;
+            activeObjects.Remove(item);
+            passiveObjects.Enqueue(item);
+            releaseAct?.Invoke(item);
+        }
+
+        public void ReleaseAll()
+        {
+            if (ActiveObjectCount == 0) return;
+
+            for (int i = ActiveObjectCount - 1; i >= 0; i--)
+            {
+                Release(activeObjects[i]);
+            }
+        }
+
+        private T CreateItem()
+        {
+            T newItem = Object.Instantiate(objectPrefab);
+            createAct?.Invoke(newItem, this);
+            return newItem;
+        }
+    }
+}
