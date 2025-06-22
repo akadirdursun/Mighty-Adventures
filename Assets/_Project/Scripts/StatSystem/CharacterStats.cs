95a21242f82195a799b0cd4304c422b56fd3fac4
Abdülkadir Dursun <akadirdursn@gmail.com>
2025-06-22 15:34:51 +0300

    fix: Set attack speed and attack token values to constructor of CharacterStats

--- Assets/_Project/Scripts/StatSystem/CharacterStats.cs
+++ Assets/_Project/Scripts/StatSystem/CharacterStats.cs
@@ -8,12 +8,20 @@ namespace MightyAdventures.StatSystem
     {
         #region Constructors
 
-        public CharacterStats(VitalityStat health, SimpleStat healthRegen, PercentileStat damageResistance, IntervalStat damage)
+        public CharacterStats(
+            VitalityStat health, 
+            SimpleStat healthRegen, 
+            PercentileStat damageResistance, 
+            IntervalStat damage,
+            SimpleStat attackSpeed,
+            SimpleStat attackTokenCount)
         {
             this.health = health;
             this.healthRegen = healthRegen;
             this.damageResistance = damageResistance;
             this.damage = damage;
+            this.attackSpeed = attackSpeed;
+            this.attackTokenCount = attackTokenCount;
         }
 
         #endregion
@@ -22,16 +30,26 @@ namespace MightyAdventures.StatSystem
         [SerializeField] private SimpleStat healthRegen;
         [SerializeField] private PercentileStat damageResistance;
         [SerializeField] private IntervalStat damage;
+        [SerializeField] private SimpleStat attackSpeed;
+        [SerializeField] private SimpleStat attackTokenCount;
 
         public VitalityStat Health => health;
         public SimpleStat HealthRegen => healthRegen;
         public PercentileStat DamageResistance => damageResistance;
         public IntervalStat Damage => damage;
+        public SimpleStat AttackSpeed => attackSpeed;
+        public SimpleStat AttackTokenCount => attackTokenCount;
 
 
         public CharacterStats Clone()
         {
-            return new CharacterStats(health.Clone(), healthRegen.Clone(), damageResistance.Clone(), damage.Clone());
+            return new CharacterStats(
+                health.Clone(), 
+                healthRegen.Clone(), 
+                damageResistance.Clone(), 
+                damage.Clone(), 
+                attackSpeed.Clone(),
+                attackTokenCount.Clone());
         }
     }
 }
