ba38423110a9736fa1ded77f1cbde170ffeb56b0
Abdülkadir Dursun <akadirdursn@gmail.com>
2025-06-25 19:44:15 +0300

    feat: Add abstract power up data

---
+++ Assets/_Project/Scripts/PowerUps/AbstractPowerUpData.cs
@@ -0,0 +1,14 @@
+﻿using MightyAdventures.StatSystem;
+using UnityEngine;
+
+namespace MightyAdventures.PowerUps
+{
+    public abstract class AbstractPowerUpData : ScriptableObject
+    {
+        [SerializeField] private Sprite icon;
+        public Sprite Icon => icon;
+
+        public abstract void ApplyPowerUp(PlayerCharacterStats stats);
+        public abstract string GetDescription();
+    }
+}
