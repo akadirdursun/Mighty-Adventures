14748f77eb6b2d878cbbb77e529415813e5e1f14
Abdülkadir Dursun <akadirdursn@gmail.com>
2025-06-26 10:45:56 +0300

    feat: Add simple stat power ups

---
+++ Assets/_Project/Scripts/PowerUps/SimpleStatFlatPowerUpData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MightyAdventures.StatSystem;
+using UnityEngine;
+
+namespace MightyAdventures.PowerUps
+{
+    [CreateAssetMenu(fileName = "NewSimpleStatFlatPowerUpData", menuName = "Mighty Adventures/Power Ups/Simple Stat Flat Power Up")]
+    public class SimpleStatFlatPowerUpData : AbstractPowerUpData
+    {
+        [SerializeField] private SimpleStatTypes statType;
+        [SerializeField] private float statIncrease;
+
+        public override void ApplyPowerUp(PlayerCharacterStats stats)
+        {
+            var simpleStat = GetStat();
+            simpleStat.ChangeValue(statIncrease);
+
+            SimpleStat GetStat()
+            {
+                return statType switch
+                {
+                    SimpleStatTypes.AttackSpeed => stats.AttackSpeed,
+                    SimpleStatTypes.SkillTokenCount => stats.MaxSkillTokenCount,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }
+        }
+
+        public override string GetDescription()
+        {
+            var colorHex = GetColorHexCode();
+            return $"Increase <color=#{colorHex}>{statType}</color> by {statIncrease}";
+        }
+    }
+}
